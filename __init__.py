d5fb6d129b05c45a72e86f197579d3c03d4d112f
Jacob Puthipiroj <thetruejacob@users.noreply.github.com>
2020-12-01 00:33:14 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ def reprocess(card):
         # card is already due
         card.queue = QUEUE_TYPE_REV
         card.type = CARD_TYPE_REV 
-        showInfo(f"This card is overdue. The next interval will be {card.ivl}")
+        # showInfo(f"This card is overdue. The next interval will be {card.ivl}")
         card.ivl = 1
         card.due = card.col.sched.today # TODO: find real day  # Originally card.due = self.today
         # showInfo(f"Setting its due date to today since already due.")
